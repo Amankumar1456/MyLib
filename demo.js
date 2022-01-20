ea480dcf3c295bcfc0c9a477a224e8afa6434d8d
amankumar1456 <amankumar1456@gmail.com>
2022-01-20 12:26:51 +0530

    pug-trail

--- demo.js
+++ demo.js
@@ -10,8 +10,9 @@ const app=express();
 dotenv.config();
 
 
-// pug 
-app.set('view engine', 'pug');
+const path = require("path"); 
+
+
 
 //#region db connection to cloud
 
@@ -28,6 +29,29 @@ mongoose.connect(process.env.DB_CONNECT,{
 app.use(bodyParser.json());
 app.use("/",Router);
 
+//#region pug-test
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+// controller 
+app.use("/pugTest",(req,res)=>{
+  res.render("index", { title: "This is title" });
+  // second argument  {optional}  {object ->passes to the template}. become local variables in the template. 
+  // However, index.pug doesn't use title locally; instead, the template it extends,layout.pug, uses it.
+
+  //res.status(200).send("WHATABYTE: Food For Devs");
+})
+
+app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+}); 
+// The views setting tells Express what directory it should use as the source of view template files. In this case, you set the views directory as the source using the path.join() method, which creates a cross-platform file path.
+
+// The view engine setting tells Express what template engine to use, which in this case, is pug.
+//#endregion
 app.listen(3000, () => {
   console.log("Server is running at port 3000");
 
--- routes/routes.js
+++ routes/routes.js
@@ -9,33 +9,41 @@ app.get("/get-all",async (req,res)=>{
         console.log("Getting all the books")
         const book=await Books.find();
         console.log("book vale find ke niche");
-        //res.json(book);
+       
         //const query = await axios.get('http://localhost:3000/get-all');
-        res.render("example")
+        res.render("index")
+        //res.json(book);
 
     } catch (error) {
         console.log(error);
         res.json(error);
     }
 });
+
 //app.get("/get-all",BookController.book_all);
-app.get('/', async (req, res) => {
-    //const query = await axios.get('http://localhost:3001/results');
-    res.render('index', { employees: query.data });
-  });
+// app.get('/', async (req, res) => {
+//     const query = await axios.get('http://localhost:3001/results');
+//     res.render('index', { employees: query.data });
+//   });
 
 app.put("/add-book",async (req,res)=>{
-    console.log(req);
-    const books={
-        title:req.body.title,
-        author:req.body.author,
-        genere:req.body.genere
+    try{
+        //console.log(req);
+        let books={
+            title:req.body.title,
+            author:req.body.author,
+            genere:req.body.genere
+        }
+        const book=new Books(books);
+        await book.save();
+        console.log("book saved");
+        //res.json(book)
+        res.send(book)
     }
-    const book=new Books(books);
-    await book.save();
-    console.log("book saved");
-    //res.json(book)
-    res.send(book)
+    catch(error)
+    {
+        console.log("couldnt add book...");
+    }
 })
 app.put("/detail/:title",async(req,res)=>{
     try {
@@ -58,17 +66,18 @@ app.get("/update/:title",async (req,res)=>{
 });
 app.get("/isAwake",async (req,res)=>{
     res.send("I am up...")
-})
+});
 app.put("/delete/:title",async(req,res)=>{
     try {
         var title=req.params.title;
-        Books.findOneAndRemove({"title":title})
+        Books.remove({"title":title})
+        console.log("book deleted "+title)
         res.send()
     } catch (error) {
         console.error(error);
 
     }
-})
+});
 
 
 module.exports=app;
